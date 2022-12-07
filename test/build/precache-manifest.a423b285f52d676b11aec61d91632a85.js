84fef06ea6d7bd6884b8fbef6cc305d4abd5ebc4
Tosin <insanechills@gmail.com>
2022-12-07 12:42:42 -0600

    build folder

---
+++ test/build/precache-manifest.a423b285f52d676b11aec61d91632a85.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1abe4f75706bfbc4c13",
+    "url": "/static/css/main.b58297b8.chunk.css"
+  },
+  {
+    "revision": "a1abe4f75706bfbc4c13",
+    "url": "/static/js/main.a1abe4f7.chunk.js"
+  },
+  {
+    "revision": "7b9a8881c0972fa68d47",
+    "url": "/static/css/1.bed24ce7.chunk.css"
+  },
+  {
+    "revision": "7b9a8881c0972fa68d47",
+    "url": "/static/js/1.7b9a8881.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a01a62c0dde3bf4df8242839dab4511a",
+    "url": "/static/media/logo.a01a62c0.png"
+  },
+  {
+    "revision": "a01a62c0dde3bf4df8242839dab4511a",
+    "url": "/static/media/spinLogo.a01a62c0.png"
+  },
+  {
+    "revision": "6b71c4e6ad9551beab166c0734fac4e4",
+    "url": "/static/media/Pizza.6b71c4e6.png"
+  },
+  {
+    "revision": "40b0f7604d3e3327ebc3ddda9f327f1d",
+    "url": "/static/media/bacon.40b0f760.png"
+  },
+  {
+    "revision": "0e69981b491c1fc813adbc63f610ec1b",
+    "url": "/static/media/Banana_Peppers.0e69981b.png"
+  },
+  {
+    "revision": "e86c560633e850c4cc979dfdb39dba7f",
+    "url": "/static/media/Black_Forest_Ham.e86c5606.png"
+  },
+  {
+    "revision": "e56d71943d529f6a9d861b6088675ebf",
+    "url": "/static/media/Black_Olives.e56d7194.png"
+  },
+  {
+    "revision": "ef8b87f93a445ffe4a2f556b886d116d",
+    "url": "/static/media/brocolli.ef8b87f9.png"
+  },
+  {
+    "revision": "21685f768d87d20ae7971918793f26f1",
+    "url": "/static/media/Caramelized_Onion.21685f76.png"
+  },
+  {
+    "revision": "810109b3da5bca59e9d110248ce69bc2",
+    "url": "/static/media/Cherry_Tomato.810109b3.png"
+  },
+  {
+    "revision": "2159ac60d869bf288072f41579970da7",
+    "url": "/static/media/garlic.2159ac60.png"
+  },
+  {
+    "revision": "b41a12d6d8fbf75cb20731d4075ba5da",
+    "url": "/static/media/Green_Peppers.b41a12d6.png"
+  },
+  {
+    "revision": "a769cf4e17e457ed684f6743528fa785",
+    "url": "/static/media/Italian_Sausage.a769cf4e.png"
+  },
+  {
+    "revision": "189b980a4dac957f5cf9a6a068e29392",
+    "url": "/static/media/jalapenos.189b980a.png"
+  },
+  {
+    "revision": "eb382b7662b52b5cd032bd3f3f6d615b",
+    "url": "/static/media/Kalmata_Olives.eb382b76.png"
+  },
+  {
+    "revision": "cf0d4d6e777a55d70c92670d2240064b",
+    "url": "/static/media/Meatball.cf0d4d6e.png"
+  },
+  {
+    "revision": "08974fb9720e24be9e061ca92de090ee",
+    "url": "/static/media/mushrooms.08974fb9.png"
+  },
+  {
+    "revision": "c2ff50fd76754f74630ba69e6ffcdf9e",
+    "url": "/static/media/Pepperoni.c2ff50fd.png"
+  },
+  {
+    "revision": "d512acdd67f154b4caf0a5b3c6f99223",
+    "url": "/static/media/Red_Onions.d512acdd.png"
+  },
+  {
+    "revision": "494ddc84bf6d33d58028fd966b49edb0",
+    "url": "/static/media/Red_Potatoes.494ddc84.png"
+  },
+  {
+    "revision": "5ca7f208cc59d88f058cb4376c6463a1",
+    "url": "/static/media/Red_Peppers.5ca7f208.png"
+  },
+  {
+    "revision": "6533f20bcdbebd1be22891d7a5e9961d",
+    "url": "/static/media/salami.6533f20b.png"
+  },
+  {
+    "revision": "d01cb63c638b72292bda2a7558fd7a62",
+    "url": "/static/media/Smoked_Chicken.d01cb63c.png"
+  },
+  {
+    "revision": "9a5950c56a5b640fb1ea62fe13aaeb35",
+    "url": "/static/media/spinach.9a5950c5.png"
+  },
+  {
+    "revision": "06a748814db755f1b626195d8eb40e02",
+    "url": "/static/media/pizzaStock.06a74881.jpeg"
+  },
+  {
+    "revision": "d4544dbc1e44ab2efdee9892a3f76874",
+    "url": "/static/media/pizza_home.d4544dbc.jpg"
+  },
+  {
+    "revision": "54bfd527b6612e6bb3c360e4f5e0311c",
+    "url": "/index.html"
+  }
+];
