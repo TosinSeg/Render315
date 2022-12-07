d44db541dc79224269984393f323776eb71ef3fd
Tosin <insanechills@gmail.com>
2022-12-07 16:44:46 -0600

    port fix

--- test/src/database.js
+++ test/src/database.js
@@ -3,15 +3,15 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv').config();
 // Create express app
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 //app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(5001, () => {
-    console.log("server has started on port 5001");
+app.listen(port, () => {
+    console.log("server has started on port: " + PORT);
 })
 
 // Create pool
